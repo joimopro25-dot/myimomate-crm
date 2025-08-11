e65e94db3d2b31435a0dc0e6a121e63364381122
Joaquim Oliveira <jo.imopro.25@gmail.com>
2025-08-11 17:37:07 +0100

    Criar espinha dorsal

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/app': path.resolve(__dirname, './src/app'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+      '@/components': path.resolve(__dirname, './src/shared/components'),
+      '@/hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@/services': path.resolve(__dirname, './src/shared/services'),
+      '@/utils': path.resolve(__dirname, './src/shared/utils'),
+      '@/types': path.resolve(__dirname, './src/shared/types'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   server: {
     port: 3000,
-    host: true,
-    open: true
+    open: true,
   },
-  root: '.',
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    sourcemap: true,
+  },
 })
